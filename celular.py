17c651d6af6f2100bfd1f60d4d155c8b18767d72
tulio-albuquerque <thulle777@gmail.com>
2014-03-16 02:22:13 -0300

    Create celular.py

---
+++ celular.py
@@ -0,0 +1,15 @@
+def letra(a):
+    if(a=='a'):
+        return '2'
+    elif(a=='b'):
+        return '22'
+    elif(a=='c'):
+        return '222'
+    elif(a=='d'):
+        return '3'
+    elif(a=='e'):
+        return '33'
+    elif(a=='f'):
+        return '333'
+    else:
+        return '9999'
